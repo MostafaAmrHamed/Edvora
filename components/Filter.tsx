7c28d9d5ad1eedc57670781b0f5f60e085dba03e
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-02-08 14:54:39 +0200

    fix(filter): color text

--- components/Filter.tsx
+++ components/Filter.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Filter = () => {
   return (
     <div className="h-fit w-[200px] rounded-2xl bg-primary-2 p-5 text-white">
-      <h1 className="text-subtex mb-7 border-b-[1px] border-subtext pb-2 text-[#A5A5A5]">
+      <h1 className="text-subtex mb-7 border-b-[1px] border-subtext pb-2 font-light text-[#A5A5A5]">
         Filters
       </h1>
       <select className="mb-3 w-full rounded bg-primary-1 p-1">
