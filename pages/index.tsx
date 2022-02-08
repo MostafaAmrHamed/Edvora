d6586b72779ee7011e8ba5f8cde120fbb3b156f7
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-02-08 13:04:09 +0200

    feat(home): designed the template of UI

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,14 +1,22 @@
 import Head from 'next/head'
-
+import Filter from '../components/Filter'
 export default function Home() {
   return (
-    <div className="flex min-h-screen flex-col py-2">
+    <div className="container mx-auto flex min-h-screen flex-col p-5 xl:px-0">
       <Head>
-        <title>Create Next App</title>
+        <title>Edvora</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <h1 className="text-3xl font-semibold text-white">Edvora</h1>
+        <div className="flex flex-col gap-5 md:flex-row">
+          <Filter />
+          <div>
+            <h1 className="mb-4 text-3xl font-semibold text-white">Edvora</h1>
+            <h1 className="mb-4 text-xl text-subtext">Products</h1>
+            <div>Slider 1</div>
+            <div>Slider 2</div>
+          </div>
+        </div>
       </div>
     </div>
   )
