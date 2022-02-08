ba8e0ef9649ed38e7e46409d0fb938aa87060f72
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-02-08 17:21:21 +0200

    fix(home): grid columns

--- pages/index.tsx
+++ pages/index.tsx
@@ -10,11 +10,11 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <div>
-        <div className="lg:grid lg:grid-cols-8 lg:gap-5">
-          <div className="mb-10 lg:mb-0">
+        <div className="lg:grid lg:grid-cols-12 lg:gap-5">
+          <div className="mb-10 lg:col-span-2 lg:mb-0">
             <Filter />
           </div>
-          <div className="lg:col-span-6 lg:col-start-3">
+          <div className="lg:col-span-10 lg:col-start-4">
             <h1 className="mb-4 text-4xl font-bold text-[#FFFFFFDE]">Edvora</h1>
             <h1 className="mb-5 text-2xl font-medium text-subtext">Products</h1>
             <div className="mb-5">
