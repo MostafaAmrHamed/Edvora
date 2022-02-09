118f3754eaac0965972ab9094ce1b5bd5b013c35
MostafaAmrHamed <mostafaamro5619@gmail.com>
2022-02-09 18:52:32 +0200

    added product types

---
+++ types/index.ts
@@ -0,0 +1,23 @@
+export type product = {
+  product_name: string
+  brand_name: string
+  price: number
+  address: { state: string; city: string }
+  discription: string
+  date: string
+  time: string
+  image: string
+}
+export type products = {
+  brand_name: string
+  items: {
+    product_name: string
+    brand_name: string
+    price: number
+    address: { state: string; city: string }
+    discription: string
+    date: string
+    time: string
+    image: string
+  }[]
+}
